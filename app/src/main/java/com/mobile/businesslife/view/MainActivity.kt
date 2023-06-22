ac4c3939e0d66cc5d946a94aa79143dab53b09bd
Mustafa <yazilimcimustafa33@gmail.com>
2023-06-23 00:10:36 +0300

    Added network state checker.

--- app/src/main/java/com/mobile/businesslife/view/MainActivity.kt
+++ app/src/main/java/com/mobile/businesslife/view/MainActivity.kt
@@ -2,16 +2,19 @@ package com.mobile.businesslife.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.mobile.businesslife.R
 import com.mobile.businesslife.databinding.ActivityMainBinding
 import com.mobile.businesslife.fragments.main.CryptoFragment
 import com.mobile.businesslife.fragments.main.EverythingFragment
 import com.mobile.businesslife.fragments.main.TopHeadlinesFragment
+import com.mobile.businesslife.service.checkNetwork.CheckNetwork
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private var networkState : CheckNetwork? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +22,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        replaceFragment(TopHeadlinesFragment())
+        networkState = CheckNetwork()
+
+        if(networkState!!.checkForInternet(this)){
+            replaceFragment(TopHeadlinesFragment())
+            changeBottomItem()
+        }else{
+            Toast.makeText(this,"There is no connection!",Toast.LENGTH_SHORT).show()
+        }
 
         //API Key -> 4dddd76a8883407eabfa37a58c38465d
 
         //NewsAPI (Everything) -> https://newsapi.org/v2/everything?domains=wsj.com&apiKey=4dddd76a8883407eabfa37a58c38465d
         //NewsAPI (Top Headlines) -> https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4dddd76a8883407eabfa37a58c38465d
 
+
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout,fragment)
+        transaction.commit()
+    }
+
+    private fun changeBottomItem(){
         binding.mainBottomNavigation.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.topHeadLines -> {
@@ -49,11 +69,5 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
-    }
-
-    private fun replaceFragment(fragment : Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frameLayout,fragment)
-        transaction.commit()
     }
 }
